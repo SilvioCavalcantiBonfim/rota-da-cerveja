8f2e839732cbd88a5ed0463c4b3238f278fdc836
RobertoVitor7 <robertovitor6666@gmail.com>
2024-12-10 19:43:14 -0300

    add image

--- src/app/component/recommendation/beerCard/beerCard.tsx
+++ src/app/component/recommendation/beerCard/beerCard.tsx
@@ -12,7 +12,7 @@ export type BeerProps = {
 export const BeerCard: React.FunctionComponent<BeerProps> = (props: BeerProps) => {
   return  <Link className="beer-card" href={`/beer/${sanitize(props.name)}`}>
   <div className="beer-card-image-contanier">
-    <img src={props.src || "https://via.placeholder.com/300x200"} alt="Cerveja Artesanal" className="beer-image" />
+    <img src={props.src || "/api/image?name=cervejaria-rio-cristalino&type=breweries&color=colorful"} alt="Cerveja Artesanal" className="beer-image" />
   </div>
   <h2 className="beer-name mt-2">{props.name}</h2>
   <p className="beer-description">{props.description}</p>
